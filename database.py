d2e0fcaeb10976d2387d8d98a2c35cda92cb5ce9
coli-bear <iln1027@kakao.com>
2025-05-22 19:23:00 +0900

    fix: database autocommit 을 위한 데코레이터 Session 가져오기 기능 개선

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
+# TODO : Move to environment variable
 DATABASE_SOURCE = "postgresql://postgres:postgres@localhost:5432/fastapi_db"
 
 engine = create_engine(DATABASE_SOURCE)
@@ -10,13 +11,37 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
+def __kwargs_session_get(kwargs):
+    for key, value in kwargs.items():
+        if isinstance(value, Session):
+            return value
+    return None
+
+
+def __args_session_get(args):
+    for arg in args:
+        if isinstance(arg, Session):
+            return arg
+    return None
+
+
+def __session_get(args, kwargs):
+    db = __args_session_get(args)
+    if db is None:
+        db = __kwargs_session_get(kwargs)
+    return db
+
+
 def auto_commit(func):
     def wrapper(*args, **kwargs):
         try:
-            db = kwargs.get('db')
+            db = __session_get(args, kwargs)
         except AttributeError:
-            raise AttributeError('You need to define db attribute')
+            raise AttributeError('You need to define database session attribute')
 
+        if db is None:
+            raise AttributeError('You need to define database session attribute')
         try:
             result = func(*args, **kwargs)
             db.commit()
@@ -24,7 +49,9 @@ def auto_commit(func):
         except Exception as e:
             db.rollback()
             raise e
+
     return wrapper
+
 
 def get_db():
     db = SessionLocal()
